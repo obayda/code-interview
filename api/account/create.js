5d3e22991dd2f089276747cd12341876ba79881f
obayda <obayda7h@gmail.com>
2019-09-02 16:26:43 +0300

    Add email validation, exist email will give error response message

--- api/account/create.js
+++ api/account/create.js
@@ -1,8 +1,14 @@
 const Account = require('../../models/account/Account');
 
-module.exports = async function(req, res, next) {
-	const {email, name, age} = req.body;
-	const account = new Account({email, name, age});
-	await account.save();
-	return res.send({message: 'success'});
+module.exports = async function (req, res, next) {
+	const { email, name, age } = req.body;
+	Account.find({ email: email }, function (err, data) {
+		if (data.length) {
+			res.send({ error: "email already exists" })
+		} else {
+			const account = new Account({ email, name, age });
+			account.save();
+			res.send({ message: 'success' });
+		}
+	});
 };
